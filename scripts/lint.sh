ad7bbcd6bcc3d40ed73c51651d3fc122efbf3768
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-15 17:45:12 +0530

    Remove non existing directory from linting script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Running pyup_dirs..."
-pyup_dirs --py38-plus --recursive autogen_agents examples tests
+pyup_dirs --py38-plus --recursive autogen_agents tests
 
 echo "Running ruff..."
-ruff autogen_agents examples tests --fix
+ruff autogen_agents tests --fix
 
 echo "Running black..."
-black autogen_agents examples tests
+black autogen_agents tests
